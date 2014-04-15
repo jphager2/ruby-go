be7d4fdf7b9d9c40691cd77b9cd7cc38044f97c6
jphager2 <jphager2@gmail.com>
2014-04-15 14:24:26 +0200

    initial commit

---
+++ lib/board.rb
@@ -0,0 +1,206 @@
+class GoBoard 
+  Color = {black: "x", white: "o", empty: "-"}
+
+  attr_accessor :moves
+
+  def initialize(size)
+    @board = []
+    (1..size).each_with_object(@board) do |row_num, board| 
+      board << (1..size).each_with_object([]) do |col_num, row| 
+        row << Stone.new(col_num, row_num)
+      end
+    end
+
+    @moves = []
+  end
+
+  def to_str
+    board = ""
+    @board.each {|row| board << row.join(" ") << "\n"}
+    return board
+  end
+
+  def to_s 
+    to_str 
+  end
+
+  def to_ary
+    @board
+  end
+
+  def to_a 
+    to_ary 
+  end
+
+  def at(x, y)
+    @board[y][x]
+  end
+
+  def stones_around(x, y)
+    stones = [] 
+    stones << @board[y][x-1] unless x == 0
+    stones << @board[y][x+1] unless x == @board.length - 1
+    stones << @board[y-1][x] unless y == 0
+    stones << @board[y+1][x] unless y == @board.length - 1
+    stones
+  end
+
+  def place_stone(stone)
+    stone.place_on_board(self)
+    @moves << stone
+  end
+
+  def liberties(stone)
+    group = get_stone_group(stone)
+
+    group.each_with_object([]) do |stone, libs| 
+      libs << stone.liberties(self)
+    end.flatten.uniq.count
+  end
+
+  def get_stone_group(stone)
+    stone.get_group(self)
+  end
+end
+
+class Stone
+
+  attr_reader :color, :always_placable
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @color = :empty 
+    @always_placable = true
+  end
+
+  def ==(other)
+    self.class == other.class and self.color == other.color
+  end
+
+  def to_cord 
+    [@x, @y]
+  end
+
+  def to_str
+    GoBoard::Color[@color]
+  end
+
+  def to_s 
+    to_str 
+  end
+
+  def place_on_board(board)
+    unless board.to_a[@y][@x].always_placable or @always_placable
+      raise PlacementError 
+    end
+    board.to_a[@y][@x] = self
+  end
+
+  def get_group(board, group = [])
+    return [self] if @color == :empty 
+
+    stones_around = board.stones_around(*self.to_cord)
+
+    group << self
+    stones_around.each do |stone|
+
+      if stone.color == self.color
+        unless group.any? {|s| s.object_id == stone.object_id}
+          group += stone.get_group(board, group)
+        end
+      end
+    end
+
+    group.flatten.uniq
+  end
+
+
+  def liberties(board)
+    return [self] if @color == :empty 
+     
+    board.stones_around(*self.to_cord).each_with_object([]) do |s, libs| 
+      libs << s if s.color == :empty 
+    end 
+  end
+end
+
+class BlackStone < Stone 
+
+  def initialize(x, y)
+    super
+    @color = :black
+    @always_placable = false
+  end
+end
+
+class WhiteStone < Stone 
+
+  def initialize(x, y)
+    super
+    @color = :white
+    @always_placable = false
+  end
+end
+
+class Stone::PlacementError < Exception
+end
+
+class GoGame 
+
+  attr_reader :board, :captures
+
+  def initialize(options)
+    @board = GoBoard.new(options[:board])
+    @captures = []
+  end
+
+  def place_white_stone(x, y)
+    place_stone(WhiteStone.new(x,y))
+  end
+
+  def place_black_stone(x, y)
+    place_stone(BlackStone.new(x,y))
+  end
+
+  def print_board
+    puts @board.to_s
+  end
+
+  private
+    def place_stone(stone)
+      bad_cord = stone.to_cord.any? do |cord| 
+        cord < 0 or cord > @board.to_a.length 
+      end
+
+      if bad_cord
+        raise ArgumentError, 
+          "Coordinates need to be between 0..#{@board.to_a.length}" 
+      end
+
+      @board.place_stone(stone) #temporarily
+      check_capture
+
+      print_board
+    end
+
+    def check_capture
+      last_stone = @board.moves.last 
+      
+      @board.stones_around(*last_stone.to_cord).each do |stone|
+        capture_group(stone) if board.liberties(stone) == 0
+      end
+    end
+
+    def capture_group(stone)
+      @board.get_stone_group(stone).each do |stone|
+        capture(stone)
+      end
+    end
+
+    def capture(stone)
+      @captures << stone 
+      x, y = stone.to_cord
+      @board.place_stone(Stone.new(x, y))
+    end
+end
---
+++ test/board.rb
@@ -0,0 +1,69 @@
+require_relative '../lib/board'
+
+require 'minitest/autorun'
+require 'stringio'
+
+class GoBoardTest < MiniTest::Unit::TestCase
+  @x = GoBoard::Color[:black]
+  @y = GoBoard::Color[:white]
+  @_ = GoBoard::Color[:empty]
+  EmptyBoardx9 = "#{(@_ * 9).split('').join(' ')}\n" * 9
+
+  def test_board_is_empty_when_initialized
+    board = GoBoard.new(9)
+    temp = $stdout
+    $stdout = StringIO.new
+    puts board.to_s
+    $stdout.rewind 
+    assert_includes $stdout.read, EmptyBoardx9
+  end 
+
+  def test_place_stone_on_board
+    board = GoBoard.new(9)
+    stone = BlackStone.new(3,3)
+    board.place_stone(stone)
+    assert_equal board.to_a[3][3], stone 
+  end
+
+  def test_cannot_place_one_stone_on_another
+    board = GoBoard.new(9)
+    stone = BlackStone.new(3,3)
+    board.place_stone(stone)
+    assert_raises(Stone::PlacementError) { board.place_stone(stone) }
+  end
+end
+
+class GoGameTest < MiniTest::Unit::TestCase 
+  def capture
+    game = GoGame.new(board: 9)
+    game.place_black_stone(2,2)
+    game.place_white_stone(2,1)
+    game.place_white_stone(2,3)
+    game.place_white_stone(1,2)
+    game.place_white_stone(3,2)
+    game
+  end
+  
+  def test_capture_removes_stone
+    game = capture
+    assert_equal Stone.new(2,2), game.board.at(2,2) 
+  end
+
+  def capture_two
+    game = GoGame.new(board: 9)
+    game.place_black_stone(2,2)
+    game.place_black_stone(2,3)
+    game.place_white_stone(2,1)
+    game.place_white_stone(2,4)
+    game.place_white_stone(1,2)
+    game.place_white_stone(1,3)
+    game.place_white_stone(3,2)
+    game.place_white_stone(3,3)
+    game
+  end
+
+  def test_capture_two_stones
+    game = capture_two
+    assert_equal 2, game.captures.length 
+  end 
+end
